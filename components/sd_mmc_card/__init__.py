891872f51f5bb961ee626699937abb045da0bd56
KrX3D <KrX3D@users.noreply.github.com>
2026-02-02 16:03:56 +0100

    3

---
+++ components/sd_mmc_card/__init__.py
@@ -0,0 +1,7 @@
+# components/sd_mmc_card/__init__.py
+# Minimal __init__.py for ESPHome external component.
+# This file can be empty, but having it prevents "Could not find __init__.py".
+# If you later want to add YAML schema/codegen, put it here.
+
+# Optionally export a version:
+__version__ = "0.1.0"
