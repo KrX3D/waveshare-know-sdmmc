87353982a2c71092a59506a83e107b94d491b269
KrX3D <KrX3D@users.noreply.github.com>
2026-02-02 17:22:19 +0100

    14

--- components/sd_mmc_card/text_sensor.py
+++ components/sd_mmc_card/text_sensor.py
@@ -5,20 +5,23 @@ from . import SdMmcCard
 from esphome.const import CONF_ID, CONF_NAME, CONF_TYPE
 
 CONF_TYPE = "type"
-TYPES = ["sd_card_type", "file_content"]  # supported text sensor types
+TYPES = ["sd_card_type", "file_content"]
 
 CONFIG_SCHEMA = cv.All(
     cv.Schema({
-        cv.GenerateID(): cv.use_id(SdMmcCard),  # use SD card instance directly
+        cv.GenerateID(): cv.use_id(SdMmcCard),
         cv.Required(CONF_TYPE): cv.one_of(*TYPES),
         cv.Required(CONF_NAME): cv.string,
     })
 )
 
 async def to_code(config):
-    sd = await cg.get_variable(config[cv.GenerateID()])  # SD card instance
-    ts = text_sensor.new_text_sensor(config[CONF_NAME])
+    sd = await cg.get_variable(config[cv.GenerateID()])
 
+    ts = cg.new_Pvariable(config[cv.GenerateID()])
+    await text_sensor.register_text_sensor(ts, config[CONF_NAME])
+
+    # Attach the text sensor to the SD card component
     if config[CONF_TYPE] == "sd_card_type":
         cg.add(sd.register_card_type_text_sensor(ts))
     elif config[CONF_TYPE] == "file_content":
