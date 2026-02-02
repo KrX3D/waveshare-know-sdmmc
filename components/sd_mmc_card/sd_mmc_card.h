cffe2e04b95b67dd7653c1fc71ab18eda4edba5a
KrX3D <KrX3D@users.noreply.github.com>
2026-02-03 00:28:52 +0100

    Require sd_mmc_card_id for sensors

--- components/sd_mmc_card/sd_mmc_card.h
+++ components/sd_mmc_card/sd_mmc_card.h
@@ -47,6 +47,8 @@ class SdMmcCard : public Component {
   bool append_file(const std::string &path, const std::string &data);
   bool read_file(const std::string &path, std::string &out);
   bool delete_file(const std::string &path);
+  bool format_card();
+  bool remount_card();
 
   bool create_directory(const std::string &path);
   bool remove_directory(const std::string &path);
@@ -68,6 +70,7 @@ class SdMmcCard : public Component {
   void register_total_space_sensor(sensor::Sensor *s) { total_space_sensor_ = s; }
   void register_used_space_sensor(sensor::Sensor *s) { used_space_sensor_ = s; }
   void register_free_space_sensor(sensor::Sensor *s) { free_space_sensor_ = s; }
+  void register_frequency_sensor(sensor::Sensor *s) { frequency_sensor_ = s; }
   void register_file_size_sensor(sensor::Sensor *s, const std::string &path) {
     file_size_sensor_ = s;
     file_size_path_ = path;
@@ -76,11 +79,17 @@ class SdMmcCard : public Component {
   // text sensor registration
   void register_card_type_text_sensor(text_sensor::TextSensor *ts) { card_type_sensor_ = ts; }
   void register_file_content_text_sensor(text_sensor::TextSensor *ts) { file_content_sensor_ = ts; }
+  void register_fs_type_text_sensor(text_sensor::TextSensor *ts) { fs_type_sensor_ = ts; }
 
  protected:
+  bool mount_card_();
+  bool unmount_card_();
+  void update_card_info_();
+
   void scan_dir_(const std::string &path, uint8_t depth, std::vector<FileInfo> &out);
   void update_sensors_();
   std::string card_type_string_();
+  std::string fs_type_string_();
 
   uint8_t clk_pin_{0}, cmd_pin_{0}, d0_pin_{0}, d1_pin_{0}, d2_pin_{0}, d3_pin_{0};
   bool mode_1bit_{false};
@@ -93,12 +102,15 @@ class SdMmcCard : public Component {
   sensor::Sensor *total_space_sensor_{nullptr};
   sensor::Sensor *used_space_sensor_{nullptr};
   sensor::Sensor *free_space_sensor_{nullptr};
+  sensor::Sensor *frequency_sensor_{nullptr};
   sensor::Sensor *file_size_sensor_{nullptr};
   std::string file_size_path_;
 
   // text sensors
   text_sensor::TextSensor *card_type_sensor_{nullptr};
   text_sensor::TextSensor *file_content_sensor_{nullptr};
+  text_sensor::TextSensor *fs_type_sensor_{nullptr};
+
 };
 
 }  // namespace sd_mmc_card
