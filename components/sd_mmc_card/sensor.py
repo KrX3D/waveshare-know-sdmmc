53acc1f8bfcf610b6f9e6d1af726f95c3a489f21
KrX3D <KrX3D@users.noreply.github.com>
2026-02-02 16:53:00 +0100

    8

--- components/sd_mmc_card/sensor.py
+++ components/sd_mmc_card/sensor.py
@@ -2,35 +2,40 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from . import SdMmcCard, sd_ns
+from esphome.const import CONF_ID, CONF_NAME, CONF_PATH, CONF_TYPE
 
-CONF_TYPE = "type"
-CONF_PATH = "path"
+CONF_SENSOR_TYPE = "type"
+SENSOR_TYPES = ["total_space", "used_space", "free_space", "file_size"]
 
-TYPES = ["total_space", "free_space", "used_space", "file_size"]
-
-sd_sensor_ns = sd_ns.namespace("sensor")
-
+# Schema for SD card sensors
 CONFIG_SCHEMA = cv.All(
     cv.Schema({
         cv.GenerateID(): cv.use_id(SdMmcCard),
-        cv.Required(CONF_TYPE): cv.one_of(*TYPES),
-        cv.Optional(CONF_PATH): cv.string,  # only for file_size
-    }),
-    cv.has_at_least_one_key(CONF_TYPE)
+        cv.Required(CONF_SENSOR_TYPE): cv.one_of(*SENSOR_TYPES),
+        cv.Required(CONF_NAME): cv.string,
+        cv.Optional(CONF_PATH): cv.string,  # required only for file_size
+    })
 )
 
 async def to_code(config):
-    var = await cg.get_variable(config[cv.GenerateID()])
-    sens = cg.new_Pvariable(config[cv.GenerateID()])
-    cg.add(var)  # ensure the component is registered
+    sd = await cg.get_variable(config[cv.GenerateID()])
+    var = cg.new_Pvariable(config[cv.GenerateID()])  # register component if not done
+    cg.add(sd)  # ensure component is registered
 
-    if config[CONF_TYPE] == "total_space":
-        cg.add(sensor.new_sensor(sens, var.total_space(), "Total SD space", "B"))
-    elif config[CONF_TYPE] == "free_space":
-        cg.add(sensor.new_sensor(sens, var.free_space(), "Free SD space", "B"))
-    elif config[CONF_TYPE] == "used_space":
-        cg.add(sensor.new_sensor(sens, var.used_space(), "Used SD space", "B"))
-    elif config[CONF_TYPE] == "file_size":
+    if config[CONF_SENSOR_TYPE] == "total_space":
+        s = sensor.new_sensor(config[CONF_NAME])
+        cg.add(sd.register_total_space_sensor(s))
+        return s
+    elif config[CONF_SENSOR_TYPE] == "used_space":
+        s = sensor.new_sensor(config[CONF_NAME])
+        cg.add(sd.register_used_space_sensor(s))
+        return s
+    elif config[CONF_SENSOR_TYPE] == "free_space":
+        s = sensor.new_sensor(config[CONF_NAME])
+        cg.add(sd.register_free_space_sensor(s))
+        return s
+    elif config[CONF_SENSOR_TYPE] == "file_size":
         path = config[CONF_PATH]
-        cg.add(sensor.new_sensor(sens, var.file_size(path), f"{path} size", "B"))
-    return sens
+        s = sensor.new_sensor(config[CONF_NAME])
+        cg.add(sd.register_file_size_sensor(s, path))
+        return s
