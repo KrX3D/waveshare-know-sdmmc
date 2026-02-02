c8ad8121028191064a0a64088207812ac5d885a3
KrX3D <KrX3D@users.noreply.github.com>
2026-02-02 16:51:50 +0100

    7

--- components/sd_mmc_card/text_sensor.py
+++ components/sd_mmc_card/text_sensor.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import text_sensor
 from . import SdMmcCard, sd_ns
+from esphome.const import CONF_NAME
 
 CONF_TYPE = "type"
 TYPES = ["sd_card_type"]
@@ -10,14 +11,14 @@ CONFIG_SCHEMA = cv.All(
     cv.Schema({
         cv.GenerateID(): cv.use_id(SdMmcCard),
         cv.Required(CONF_TYPE): cv.one_of(*TYPES),
+        cv.Required(CONF_NAME): cv.string,  # <-- add name here
     })
 )
 
 async def to_code(config):
     var = await cg.get_variable(config[cv.GenerateID()])
     ts = cg.new_Pvariable(config[cv.GenerateID()])
-    cg.add(var)  # ensure component is registered
+    cg.add(var)  # ensure the component is registered
 
-    if config[CONF_TYPE] == "sd_card_type":
-        cg.add(text_sensor.new_text_sensor(ts, var.card_type(), "SD Card Type"))
+    cg.add(text_sensor.register_text_sensor(ts, var.card_type(), config[CONF_NAME]))
     return ts
