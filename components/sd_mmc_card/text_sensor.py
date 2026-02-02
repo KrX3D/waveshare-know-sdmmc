d180388271a3918d3f6099b01a4b688f089eda50
KrX3D <KrX3D@users.noreply.github.com>
2026-02-02 17:17:20 +0100

    12

--- components/sd_mmc_card/text_sensor.py
+++ components/sd_mmc_card/text_sensor.py
@@ -2,26 +2,26 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import text_sensor
 from . import SdMmcCard
-from esphome.const import CONF_ID, CONF_NAME
+from esphome.const import CONF_ID, CONF_NAME, CONF_TYPE
 
 CONF_TYPE = "type"
-TYPES = ["sd_card_type", "file_content"]
+TYPES = ["sd_card_type", "file_content"]  # supported text sensor types
 
 CONFIG_SCHEMA = cv.All(
     cv.Schema({
-        cv.GenerateID(): cv.use_id(SdMmcCard),
+        cv.GenerateID(): cv.use_id(SdMmcCard),  # use SD card instance directly
         cv.Required(CONF_TYPE): cv.one_of(*TYPES),
         cv.Required(CONF_NAME): cv.string,
     })
 )
 
 async def to_code(config):
-    sd = await cg.get_variable(config[cv.GenerateID()])
-    ts = cg.new_Pvariable(config[cv.GenerateID()])
+    sd = await cg.get_variable(config[cv.GenerateID()])  # get SD card instance
+    ts = text_sensor.new_text_sensor(config[CONF_NAME])   # create text sensor object
 
     if config[CONF_TYPE] == "sd_card_type":
-        cg.add(text_sensor.register_text_sensor(ts, sd.card_type(), config[CONF_NAME]))
+        cg.add(sd.register_card_type_text_sensor(ts))   # attach to SD card
     elif config[CONF_TYPE] == "file_content":
-        cg.add(text_sensor.register_text_sensor(ts, sd.file_content(), config[CONF_NAME]))
+        cg.add(sd.register_file_content_text_sensor(ts))  # attach to SD card
 
     return ts
